e8a8493b1be7c52868ad25e24022ce45c5a7fb7b
Florian Bartels <Florian.Bartels@elektrobit.com>
2024-12-02 10:44:14 +0100

    Add support for alternative QNX Neutrino network stack `io-sock`

--- libc-test/build.rs
+++ libc-test/build.rs
@@ -3031,6 +3031,17 @@ fn test_neutrino(target: &str) {
     assert!(target.contains("nto-qnx"));
 
     let mut cfg = ctest_cfg();
+    if target.ends_with("_iosock") {
+        cfg.include(concat!(env!("QNX_TARGET"), "/usr/include/io-sock"));
+        headers! { cfg:
+            "io-sock.h",
+            "sys/types.h",
+            "sys/socket.h",
+            "sys/sysctl.h",
+            "net/if.h",
+            "net/if_arp.h"
+        }
+    }
 
     headers! { cfg:
         "ctype.h",
@@ -3188,6 +3199,9 @@ fn test_neutrino(target: &str) {
             // Does not exist in Neutrino
             "locale_t" => true,
 
+            // FIXME: "'__uint128' undeclared" in C
+            "__uint128" => true,
+
             _ => false,
         }
     });
@@ -3247,6 +3261,9 @@ fn test_neutrino(target: &str) {
             // Not defined in any headers.  Defined to work around a
             // stack unwinding bug.
             "__my_thread_exit" => true,
+
+            // Wrong const-ness
+            "dl_iterate_phdr" => true,
 
             _ => false,
         }
